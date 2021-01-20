c11ca3cefb5806446668c3e5671fbc09be50101a
Your Name <evifannys@gmail.com>
2021-01-20 18:40:20 +0700

    first commit

---
+++ src/a.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class a {
+    
+    public static void main(String[] args) {
+		int x= 10;
+		int y= 15;
+		System.out.println("Nilai dari A = "+ x);
+		System.out.println("Nilai dari B = "+ y);
+		}
+    
+}
---
+++ src/b.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class b {
+    public static void main(String[] args) {
+		int x= 10;
+		int y= 5 + x;
+		
+		System.out.println("Nilai dari A = "+ x);
+		System.out.println("Nilai dari B = "+ y);
+
+		
+		
+		}
+    
+}
